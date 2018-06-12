7d6953caf93f65164c251c8868aa95f6dadd0c2b
Sbmooc <olster98@gmail.com>
2018-06-12 07:41:56 +0100

    removed whitenoise storage

--- teamexchange/settings.py
+++ teamexchange/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'teamexchange.urls'
 
