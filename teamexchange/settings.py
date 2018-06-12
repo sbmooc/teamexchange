c1fc5e44e300d240379774723cd9a22565b0d63e
Sbmooc <olster98@gmail.com>
2018-06-12 08:30:28 +0100

    added https support

--- teamexchange/settings.py
+++ teamexchange/settings.py
@@ -29,6 +29,9 @@ DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Application definition
 
 INSTALLED_APPS = [
