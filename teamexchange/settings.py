434698050e73eaefbf93abd30c34ef10886e2946
Sbmooc <olster98@gmail.com>
2018-06-12 07:32:23 +0100

    changes

--- teamexchange/settings.py
+++ teamexchange/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@$w4hvgywmzft&0jo-zk!c=5^zr%dkw-$*dkxy+kj5ff4(2x!$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
