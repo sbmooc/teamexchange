ac189c8141d82eb1de628b75c9623ee5fb4b2eae
Sbmooc <olster98@gmail.com>
2018-06-12 07:14:26 +0100

    changed settings file for static

--- teamexchange/settings.py
+++ teamexchange/settings.py
@@ -53,7 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'teamexchange.urls'
 
