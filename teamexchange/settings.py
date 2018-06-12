266f99becadeaf592763f7b97f6a0d98ecaf3418
Sbmooc <olster98@gmail.com>
2018-06-12 17:11:22 +0100

    changed secret key

--- teamexchange/settings.py
+++ teamexchange/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@$w4hvgywmzft&0jo-zk!c=5^zr%dkw-$*dkxy+kj5ff4(2x!$'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
