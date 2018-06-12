594e9cdeb42552516b23ab0dcb0ce41e3125daf3
Sbmooc <olster98@gmail.com>
2018-06-12 07:09:56 +0100

    changed settings file for static

--- teamexchange/settings.py
+++ teamexchange/settings.py
@@ -152,15 +152,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = "/teamapp/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
